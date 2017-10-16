5dea27c6ca66083695b5ee192a6bfc2dca82fad6
kielm1 <martin-k@hotmail.ch>
2017-10-16 11:14:38 +0200

    changed src to github

--- Website/JS/testScript.js
+++ Website/JS/testScript.js
@@ -1,45 +1,56 @@
 function getText() {
-	
-	var txt1 = document.getElementById('bsptxt1').value;
-	
-	document.getElementById('demo').innerHTML = txt1;
+
+    var txt1 = document.getElementById('bsptxt1').value;
+
+    document.getElementById('demo').innerHTML = txt1;
 }
 
-/*Text von Textbox in myFile speichern*/
-function exportToFile() {
-	
-	var txt1 = document.getElementById('bsptxt1').value;
 
-	var textToSave = txt1;
-
-	var hiddenElement = document.createElement('a');
-
-	hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
-	hiddenElement.target = '_blank';
-	hiddenElement.download = 'myFile.txt';
-	hiddenElement.click();
-
-}
-
-/*Variablen für differenz ausrechnen (in millisekunden) */
-var time1;
-var time2;
+/*Variablen definieren*/
+var time1; /*für differenz ausrechnen (in millisekunden)*/
+var time2; /*für differenz ausrechnen (in millisekunden)*/
+var difference; /*time2 - time1*/
+var saveDifference; /*Array timeDifference*/
 
 /*Zeit wenn die Taste gedrückt wurde*/
 function keydownFunction() {
-	
-	time1 = new Date();
+
+    time1 = new Date();
 }
 
 /*Zeit wenn die Taste losgelassen wurde*/
 function keyupFunction() {
-	
-	time2 = new Date();
+
+    time2 = new Date();
 }
 
+/*Differenz zwischen keydown und keyup berechnen*/
 function timeDifference() {
 
-	var difference = time2.getTime() - time1.getTime(); /*getTime kriegt zeit in millisekunden*/
-	
-	document.getElementById('time').innerHTML = "Differenz: " + difference;
+    difference = time2.getTime() - time1.getTime(); /*getTime kriegt zeit in millisekunden*/
+
+    document.getElementById('time').innerHTML = "Differenz: " + difference;
+
+
+
+    for (var i = 0; i < (saveDifference.length);i++) {
+        saveDifference = [];
+        saveDifference.push(difference);
+    }
+}
+
+/*Text von Textbox in myFile speichern*/
+function exportToFile() {
+
+    /*var txt1 = document.getElementById('bsptxt1').value;*/
+    var txt1 = document.getElementById('bsptxt1').innerHTML = saveDifference;
+
+    var textToSave = txt1;
+
+    var hiddenElement = document.createElement('a');
+
+    hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'myFile.txt';
+    hiddenElement.click();
 }
