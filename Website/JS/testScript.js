ac1551c2a1ece18ad513e602b21b6fbad5aa0cf5
kielm1 <martin-k@hotmail.ch>
2017-10-19 16:00:41 +0200

    add keyCode

--- Website/JS/testScript.js
+++ Website/JS/testScript.js
@@ -4,10 +4,13 @@
 var time1; /*für differenz ausrechnen (in millisekunden)*/
 var time2; /*für differenz ausrechnen (in millisekunden)*/
 var difference; /*time2 - time1*/
-var saveDifference = []; /*Array timeDifference*/
-var testArray = ["86", "139", "98", "79", "66", "83"]; /* Array mit "Hallo" */
 var count = 0; /*Zählt die Unstimmigkeiten */
 
+var saveKey = new Array();
+var countKey = 0; /*var für saveKey Array */
+
+saveKey[0] = new Array(); /*Erste Spalte für difference */
+saveKey[1] = new Array(); /*Zweite Spalte für KeyCode */
 
 /*Zeit wenn die Taste gedrückt wurde*/
 function keydownFunction() {
@@ -22,15 +25,17 @@ function keyupFunction() {
 
     difference = time2.getTime() - time1.getTime(); /*getTime kriegt zeit in millisekunden*/
 
-    document.getElementById('time').innerHTML = "Differenz: " + difference; /*Show difference*/
+    document.getElementById('time').innerHTML = "Differenz: " + difference + "KeyCode: " + event.keyCode; /*Show difference*/
 
-    saveDifference.push(difference); /*wert dem array hinzufügen*/
+    saveKey[0][countKey] = "Differenz: " + difference;
+    saveKey[1][countKey] = "KeyCode: " + event.keyCode;
+    countKey = countKey + 1; /*Nächste Zeile */
 }
 
 /*Als txt Datei speichern*/
 function exportToFile() {
 
-    var fileText = saveDifference; /*Array*/
+    var fileText = saveKey; /*Array*/
 
     var textToSave = fileText;
 
@@ -42,7 +47,11 @@ function exportToFile() {
     hiddenElement.click();
 }
 
-/*Vergleicht die Eingabe (Latency) mit dem vorgegeben Text. Wenn Werte mehr als 25 ms auseinander -> False */
+/*Vergleicht die Eingabe (Latency) mit dem vorgegeben Text. Wenn Werte mehr als 25 ms auseinander -> False
+
+var saveDifference = []; Array timeDifference
+var testArray = ["86", "139", "98", "79", "66", "83"]; /* Array mit "Hallo"
+
 function compareLatency() {
 
     for (var i = 0; i < testArray.length; i++) {
@@ -56,4 +65,4 @@ function compareLatency() {
     } else {
         document.getElementById('difference').innerHTML = "False: " + count;
     }
-}
+} */
